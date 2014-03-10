7587a93fad97075d00a4b728ce11758456648fcd
Andrey Koleshko <ka8725@gmail.com>
2014-03-11 01:50:59 +0300

    Basic models and their relations with admin panel

--- stagings/admin.py
+++ stagings/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from stagings.models import Zone, Author, Genre, Staging, Piece, VacantZoneSeat
 
-# Register your models here.
+
+class VacantZoneSeatInlice(admin.TabularInline):
+  model = VacantZoneSeat
+
+class StagingAdmin(admin.ModelAdmin):
+  fields = ('piece', 'date', 'author', 'genre')
+  inlines = (VacantZoneSeatInlice, )
+  list_filter = ('date',)
+  list_display = ('piece', 'author', 'genre', 'date')
+
+admin.site.register(Zone)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Piece)
+admin.site.register(Staging, StagingAdmin)
--- stagings/models.py
+++ stagings/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Piece(models.Model):
+  name = models.CharField(max_length=255)
+
+  def __unicode__(self):
+    return self.name
+
+
+class Author(models.Model):
+  name = models.CharField(max_length=255)
+
+  def __unicode__(self):
+    return self.name
+
+
+class Genre(models.Model):
+  name = models.CharField(max_length=255)
+
+  def __unicode__(self):
+    return self.name
+
+
+class Zone(models.Model):
+  name = models.CharField(max_length=255)
+
+  def __unicode__(self):
+    return self.name
+
+
+class Staging(models.Model):
+  piece = models.ForeignKey(Piece)
+  author = models.ForeignKey(Author)
+  genre = models.ForeignKey(Genre)
+  date = models.DateField()
+
+  def __unicode__(self):
+    return '%s: %s' % (self.piece.name, self.date)
+
+
+class VacantZoneSeat(models.Model):
+  zone = models.ForeignKey(Zone)
+  staging = models.ForeignKey(Staging)
+  total = models.PositiveSmallIntegerField()
