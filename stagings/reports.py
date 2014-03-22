0af3081b24e25207832ca68cc0e7dfc236da3018
Andrey Koleshko <ka8725@gmail.com>
2014-03-22 16:58:29 +0300

    Refactor zones report

--- stagings/reports.py
+++ stagings/reports.py
@@ -1,5 +1,5 @@
 from django.db.models import Sum
-from stagings.models import Zone
+from stagings.models import Zone, Order
 
 class ZonesReport(object):
   def __init__(self):
@@ -17,6 +17,10 @@ class ZonesReport(object):
       'total_earning': self._total_earnings(self._report),
     })
 
+  @property
+  def report(self):
+    return self._report
+
   def _total_tickets(self, zone):
     return sum(zone.total_seats for zone in zone.stagingzone_set.all())
 
@@ -24,34 +28,23 @@ class ZonesReport(object):
     return zone.stagingzone_set.aggregate(Sum('available_seats'))['available_seats__sum']
 
   def _ordered_tickets(self, zone):
-    res = 0
-    for stagingzone in zone.stagingzone_set.all():
-      res += sum(lineitem.quantity
-                  for lineitem in stagingzone.lineitem_set.all()
-                    if lineitem.is_new())
-    return res
+    return sum(map(lambda (_, lineitem): lineitem.quantity,
+                self._stagingzone_and_lineitem(zone, Order.NEW)))
 
   def _paid_tickets(self, zone):
-    res = 0
-    for stagingzone in zone.stagingzone_set.all():
-      res += sum(lineitem.quantity
-                  for lineitem in stagingzone.lineitem_set.all()
-                    if lineitem.is_paid())
-    return res
+    return sum(map(lambda (_, lineitem): lineitem.quantity,
+                self._stagingzone_and_lineitem(zone, Order.PAID)))
 
   def _earnings(self, zone):
-    res = 0
-    for stagingzone in zone.stagingzone_set.all():
-      res += sum(lineitem.quantity * stagingzone.ticket_price
-                  for lineitem in stagingzone.lineitem_set.all()
-                    if lineitem.is_paid())
-    return res
+    return sum(map(lambda (zone, lineitem): zone.ticket_price * lineitem.quantity,
+                self._stagingzone_and_lineitem(zone, Order.PAID)))
 
-    pass
+  def _stagingzone_and_lineitem(self, zone, order_status):
+    for stagingzone in zone.stagingzone_set.all():
+      for lineitem in stagingzone.lineitem_set.filter(
+                        order__status=order_status
+                      ).all():
+        yield(stagingzone, lineitem)
 
   def _total_earnings(self, zones_report):
     return sum(zone_report['earnings'] for zone_report in zones_report)
-
-  @property
-  def report(self):
-    return self._report
